2be51e8cf48cf9f1d294fe2837e2628a2c05e773
Sampsa Särkkä <sampsa.sarkka@protonmail.com>
2023-11-29 11:22:15 +0200

    added python files

---
+++ pytest_tests.py
@@ -0,0 +1,10 @@
+import mathematics
+
+def test_add():
+    assert mathematics.add(5, 4) == 9
+
+def test_multiply():
+    assert mathematics.multiply(3, 4) == 12
+
+def test_power():
+    assert mathematics.power(2, 8) == 256
